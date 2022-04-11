5e4840c7ed2d96913f7472b033cd0420d01aed92
Matthew Daniels <themattyd@outlook.com>
2022-04-11 18:26:34 -0400

    reindex and add drinks2

--- separate-include/gallery/filter_tabs.js
+++ separate-include/gallery/filter_tabs.js
@@ -78,3 +78,32 @@
 		}, timeout);
 	}
 };
+
+  /**
+   * Menu isotope and filter
+   */
+  window.addEventListener('load', () => {
+	let menuContainer = select('.menu-container');
+	if (menuContainer) {
+	  let menuIsotope = new Isotope(menuContainer, {
+	    itemSelector: '.menu-item',
+	    layoutMode: 'fitRows'
+	  });
+ 
+	  let menuFilters = select('#menu-filters li', true);
+ 
+	  on('click', '#menu-filters li', function(e) {
+	    e.preventDefault();
+	    menuFilters.forEach(function(el) {
+		 el.classList.remove('filter-active');
+	    });
+	    this.classList.add('filter-active');
+ 
+	    menuIsotope.arrange({
+		 filter: this.getAttribute('data-filter')
+	    });
+ 
+	  }, true);
+	}
+ 
+   });
